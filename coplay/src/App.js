49dc47c30b88b01559dff9a7ca3b4608a51251ed
Rin826 <57347028+Rin826@users.noreply.github.com>
2020-01-07 13:02:59 +0200

    ui login

--- coplay/src/App.js
+++ coplay/src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+return(
+  <div>
+<form>
+<div id = "container">
+  <div id = "LogIn">CoPlay</div>
+  
+  
+
+<input type = "username" id = "name" placeholder = "username"></input>
+<input type = "password" id = "password" placeholder = "password"></input>
+<input type = "submit"  id = "submit" placeholder = "login"></input>
+
+
+
+
+</div>
+</form>
+
+
+
+</div>
+
+)
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
