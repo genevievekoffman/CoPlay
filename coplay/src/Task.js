83e524b1d8774677c8b0d188ec76c34cfd51ada7
Rin826 <57347028+Rin826@users.noreply.github.com>
2020-01-07 14:31:40 +0200

    added task stuff

---
+++ coplay/src/Task.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import './Task.css'
+
+function Task(props){
+    return(
+        <div className = 'task'>TASK EXAMPLE</div> 
+
+    
+    )
+
+
+}
+
+
+
+export default Task;
