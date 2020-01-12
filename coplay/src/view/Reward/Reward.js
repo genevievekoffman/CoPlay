4b1ececa0ed853028dc90626ade0e4c39a49df3d
theJoystick26 <57347034+theJoystick26@users.noreply.github.com>
2020-01-12 12:21:08 +0200

    deduct fix

--- coplay/src/view/Reward/Reward.js
+++ coplay/src/view/Reward/Reward.js
@@ -1,22 +1,35 @@
 import React, { useState } from "react";
 import "./Reward.css";
 
-function Reward(props){
+function Reward(props) {
     //passed an array of rewards
     const { reward, index, db } = props;
-    return( 
-        <div className = "reward" key = {index}>
+    return (
+        <div className="reward" key={index}>
             <div>
-                <button>
-                    Claim Reward
-                </button>
+                <button className="claim" onClick={() => deductPoints(reward[1], db)} class>Claim</button>
             </div>
             <div>
-                {reward[0]} <br/>
-                {reward[1]}  
+                {reward[0]} <br />
+                {reward[1]}
             </div>
         </div>
     )
+}
+
+function deductPoints(points, db) {
+    db.collection("Users").doc(sessionStorage.getItem("user")).get().then(function (doc) {
+
+        if (doc.get("totalPoints") >= points) {
+            let balance = doc.get("totalPoints") - points;
+
+            db.collection("Users").doc(sessionStorage.getItem("user")).update({
+                totalPoints: balance
+            });
+        } else{
+            console.log("You do not have enough points to claim this reward. Keep working!")
+        }
+    });
 }
 
 export default Reward;
