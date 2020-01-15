f1eabefecef02c0e5082252c876eba2552d99ab6
Darmonxx <57347116+Darmonxx@users.noreply.github.com>
2020-01-15 14:03:55 +0200

    Update Groups.js

--- coplay/src/view/pages/Groups/Groups.js
+++ coplay/src/view/pages/Groups/Groups.js
@@ -124,20 +124,8 @@ function AddGroupForm(props) {
                     />
                   </div>
                   <div className="modal-footer">
-                    <input
-                      className="btn btn-primary"
-                      type="submit"
-                      id="Save"
-                      value="Save"
-                      name="save"
-                    />
-                    <input
-                      className="btn btn-primary"
-                      id="Cancel"
-                      value="Cancel"
-                      type="button"
-                      // onClick = {props.onCancel}
-                    />
+                  <button type="button" class="btn btn-primary btn-sm" id="savee" data-dismiss="modal">Save</button>
+                  <button type="button" class="btn btn-secondary btn-sm" id="cancell" data-dismiss="modal">Cancel</button>
                   </div>
                 </form>
               </div>
