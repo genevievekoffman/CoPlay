206035961f950dce23998c9d03a095a5668a9fd3
Genevieve Koffman <genkoffman@gmail.com>
2020-01-15 10:57:27 +0200

    sorting

--- coplay/src/view/pages/LeaderBoard/LeaderBoard.js
+++ coplay/src/view/pages/LeaderBoard/LeaderBoard.js
@@ -37,47 +37,9 @@ function updateLeaderBoard(setUsersLists, db, setCounter){
             list.push(userInfo);
         })
         console.log("users list: " + list);
-        list = mergeSort(list)  
-        console.log("post merge: " + list)
+        list.sort((a,b) => a[1] - b[1]) //will return pos number if a>b 
         setUsersLists(list);
         setCounter(1);
     })
-
 }
-
-function mergeSort(arr){ //given a list of arrays formatted (name, #)
-    if(arr.length <= 1) {
-        return arr;
-    }
-
-    const mid = Math.floor(arr.length/2);
-
-    const left = arr.slice(0, mid); 
-    console.log("left: " + left) 
-    const right = arr.slice(mid)
-    console.log("right: " + right) 
-
-    return merge(
-        mergeSort(left), 
-        mergeSort(right)
-    );
-}
-
-function merge(left, right){
-    console.log("merging " + left + " and " + right)
-    let newArr = [], leftIndex = 0, rightIndex = 0;
-
-    while(leftIndex < left.length && rightIndex < right.length){
-        if(left[leftIndex[1]] < right[rightIndex[1]]){ //left is smaller 
-            newArr.push(left[leftIndex]);
-            leftIndex++;
-        } else {
-            newArr.push(right[rightIndex]);
-            rightIndex++;
-        }
-    }
-    console.log("merged to: " + newArr);
-    return newArr
-        .concat(left.slice(leftIndex))
-        .concat(right.slice(rightIndex));
-}
+ 
