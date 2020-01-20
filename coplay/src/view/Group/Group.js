24dc0beb638222cf22cb355063e952d39741c829
Tal Yaron <tal.yaron@gmail.com>
2020-01-20 12:14:10 +0200

    cont

--- coplay/src/view/Group/Group.js
+++ coplay/src/view/Group/Group.js
@@ -1,34 +1,41 @@
-import React, {useState} from 'react';
-import './Group.css'; 
- 
-function Group(props){
-    const {group, index, setGroupID, setHomePage, setName} = props;
+import React, { useState } from 'react';
+import './Group.css';
+
+//component
+import rightarrow from  '../../Sketches/Rightarrow.svg';
+import logo from '../../Sketches/logo.svg';
+
+function Group(props) {
+    const { group, index, setGroupID, setHomePage, setName } = props;
     //["name", "ID"]
-     
-    return(
-        
-        
-        <div className = "group" key = {index} onClick = { () => {
+
+    return (
+
+
+
+
+
+        <div className="groupName" onClick={() => {
             setGroupID(group[1]);
             setName(group[0])
             setHomePage(true); //changes screen 
-            }}>
-                 
-            
-            <div className = "groupName">
+        }}>
+            <div className='left_box'>
                 {group[0]}
             </div>
-            <div className = "rightBox">
-                >
+            <div className='right_box'>
+                <img src={logo} alt='rightarrow' />
             </div>
-             
-            {/* <div className = "subInfo">
-                {group[1]}
-            </div>  */}
+
         </div>
+
+
+        //     {/* <div className = "subInfo">
+        //        {group[1]}
+        //  </div>  */}
+
     )
 }
 
 export default Group;
 
- 
