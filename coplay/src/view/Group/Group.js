2729c34b84e56931161f8fd8a69a9cc560f43a47
Genevieve Koffman <genkoffman@gmail.com>
2020-01-21 11:55:57 +0200

    shows group IDS

--- coplay/src/view/Group/Group.js
+++ coplay/src/view/Group/Group.js
@@ -4,15 +4,37 @@ import "./Group.css";
 //component
 import rightarrow from "../../Sketches/Rightarrow.svg";
 
+
 function Group(props) {
   const {
     group,
     setGroupID,
     setHomePage,
-    setName 
+    setName,
+    ids //if true: show IDS, if not: don't show IDS
   } = props; 
 
-   
+   if (ids){
+    return (
+      <div
+        className="groupName"
+        onClick={() => {
+          setGroupID(group[1]);
+          setName(group[0]);
+          setHomePage(true); //changes screen
+        }}
+      >
+        <div className="left_box">{group[0]} <br></br> 
+        <h5>ID: {group[1]}</h5> 
+        </div>
+        <div className="right_box">
+          <img src={rightarrow} className="rightarrow" alt="rightarrow" />
+        </div>
+      </div>
+ 
+    );
+     
+   } else {
     return (
       <div
         className="groupName"
@@ -27,11 +49,10 @@ function Group(props) {
           <img src={rightarrow} className="rightarrow" alt="rightarrow" />
         </div>
       </div>
+    )
 
-      //     {/* <div className = "subInfo">
-      //        {group[1]}
-      //  </div>  */}
-    );
+   }
+     
   }
  
 
