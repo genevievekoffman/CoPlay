542cfaf6d3d10409d04610c158d9613b4c9374ee
theJoystick26 <57347034+theJoystick26@users.noreply.github.com>
2020-01-22 11:20:45 +0200

    ui

--- coplay/src/view/Task/Task.js
+++ coplay/src/view/Task/Task.js
@@ -8,59 +8,81 @@ function Task(props) {
   //passed an array of tasks
   const { task, index, db, groupID, setTaskDeleted, setPointsDisplay } = props;
   const [taskWasClicked, setTaskWasClicked] = useState(false);
-  const [totalCompleted, setTotalCompleted] = useState(0) //getCompCount(db,groupID,task)
+  const [totalCompleted, setTotalCompleted] = useState(0)
+  const [visible, setVisibility] = useState("hidden")
 
   //let completion = task[2];  //amount of times completed
   //const [taskCompleted, setTaskCompleted] = useState(completion);
 
   return (
-    <div
-      className="task"
-      key={index}
-       
-    >
-      <div className="aTask" onClick={() => {
-        taskClicked(task, setTaskWasClicked);
-      }}>
-        {task[0]} <br></br>
-        {taskWasClicked ? (
-          <div className="subInfoContainer">
-            <img src={pinkarrow} className="pinkarrow" alt="pinkarrow" />
+    // <div
+    //   className="task"
+    //   key={index}
+    // >
+    //   <div className="aTask" onClick={() => {
+    //     taskClicked(task, setTaskWasClicked);
+    //   }}>
+    //     {task[0]} <br></br>
+    //     {taskWasClicked ? (
+    //       <div className="subInfoContainer">
+    //         <img src={pinkarrow} className="pinkarrow" alt="pinkarrow" />
 
-            <div className="subInfo">
-              {task[1]} points <br></br>
-              completed count: {totalCompleted}
+    //         <div className="subInfo">
+    //           {task[1]} points <br></br>
+    //           completed count: {totalCompleted}
               
-                <div>
-                  {/* <div>completed by</div>
-                  <div>@{task[3]}</div> */}
-                  <div className="twoBtns">
-                    <button
-                      className="taskResetBtn"
-                      onClick={() => {
-                        completeTask(task[0], task[1], db, groupID, setTotalCompleted)
-                      }}
-                    >
-                      Complete
-                    </button>
-                    <button
-                      className="taskResetBtn"
-                      onClick={() => {
-                        deleteTaskClicked(task[0], groupID, db, setTaskDeleted);
-                      }}
-                    >
-                      Delete
-                    </button>
-                  </div>
-                </div>
+    //             <div>
+    //               {/* <div>completed by</div>
+    //               <div>@{task[3]}</div> */}
+    //               <div className="twoBtns">
+    //                 <button
+    //                   className="taskResetBtn"
+    //                   onClick={() => {
+    //                     completeTask(task[0], task[1], db, groupID, setTotalCompleted)
+    //                   }}
+    //                 >
+    //                   Complete
+    //                 </button>
+    //                 <button
+    //                   className="taskResetBtn"
+    //                   onClick={() => {
+    //                     deleteTaskClicked(task[0], groupID, db, setTaskDeleted);
+    //                   }}
+    //                 >
+    //                   Delete
+    //                 </button>
+    //               </div>
+    //             </div>
                 
-            </div>
-          </div>
-        ) : (
-          ""
-        )}
+    //         </div>
+    //       </div>
+    //     ) : (
+    //       ""
+    //     )}
+    //   </div>
+    // </div>
+
+    <div className = "taskGrid" key={index}>
+      <div className= "taskName">{task[0]}</div>
+      <button className = "complete">Complete</button>
+      <button className = "delete">Delete</button>
+      <div className= "info">i</div>
+
+      <div className="completePopUp">
+        <div>Did you really complete this task?</div>
+        <button>Yes</button>
+        <button>No</button>
       </div>
+
+      <div className="deletePopUp">
+        <div>Are you sure you want to delete this task?</div>
+        <button>Yes</button>
+        <button>No</button>
+      </div>
+      
     </div>
+
+
   );
 }
 
