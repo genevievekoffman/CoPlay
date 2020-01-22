77999a70c9930d84cd82b565693368cd5d7c9c5f
Genevieve Koffman <genkoffman@gmail.com>
2020-01-22 15:01:34 +0200

    POPUP WORKS

--- coplay/src/view/Reward/Reward.js
+++ coplay/src/view/Reward/Reward.js
@@ -2,116 +2,94 @@ import React, { useState } from "react";
 import "./Reward.css";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-import Form from "react-bootstrap/Form";
 
 //component
 import star from "../../Sketches/Star.svg";
+import Addreward from "../../Popups/AddReward/AddReward";
 
 function Reward(props) {
   //passed an array of rewards
   const { reward, index, db, groupID, setPointsDisplay } = props;
-  const [showModal, setShowModal] = useState(false);
-  const [failModal, setFailModal] = useState(false);
+  // const [showModal, setShowModal] = useState(false);
+  // const [failModal, setFailModal] = useState(false);
 
+  //for popup
+  const [isShowing, setIsShowing] = useState(false);
+  const [enoughPoints, setEnoughPoints] = useState(false);
 
-  const revealSuccessTask = () =>  
-    setShowModal(true);
  
-  const revealFailTask = () =>  
-    setFailModal(true);  
 
-  const handleCheckboxClick = () =>  
-    deductPoints(reward[1], db, revealSuccessTask, revealFailTask, groupID, setPointsDisplay);
-     
-  return ( 
-    <div className="reward" key={index} >  
-     
-      <div className="rewardBig">
-        <button className="box" onClick={handleCheckboxClick}></button>
-        {reward[0]} <br />
-      </div> 
-      <div className="rewardSmall">
-        {reward[1]}   <img src={star} className="star" alt="star" className="star" />
+  return (
+    <div>
+      <Addreward 
+      isShowing={isShowing} 
+      setIsShowing={setIsShowing} 
+      enoughPoints = {enoughPoints}
+      setEnoughPoints = {setEnoughPoints} 
+      />
+
+      <div
+        className="reward"
+        key={index}
+        onClick={() => {
+          checkEnoughPoints(db, groupID, reward, setEnoughPoints, setIsShowing,setPointsDisplay);
+        }}
+      >
+        <div className="rewardBig">
+          {reward[0]} <br />
+        </div>
+        <div className="rewardSmall">
+          {reward[1]}{" "}
+          <img src={star} className="star" alt="star" className="star" />
+        </div> 
+         
       </div>
-
-
-      <PurchaseSuccess
-        showSuccess={showModal}
-        hideSuccess={() => setShowModal(false)}
-      />
-      <PurchaseFail
-        showFailure={failModal}
-        hideFailure={() => setFailModal(false)}
-      />
-      {/* The functions above are the ones passed to the component PurchaseSuccess */}
     </div>
   );
 }
 
-//revealSuccessTask is the arrow function handleChecboxClick
-function deductPoints(points, db, revealSuccessTask, revealFailTask, groupID, setPointsDisplay) {
-  console.log("Deducting points")
-  db.collection("Groups").doc(groupID).collection("Users")
+function checkEnoughPoints(db, groupID, reward, setEnoughPoints, setIsShowing,setPointsDisplay){ //returns true(if they have enough points) or false (if they dont)
+    db.collection("Groups")
+    .doc(groupID)
+    .collection("Users")
     .doc(sessionStorage.getItem("user"))
     .get()
-    .then(function (doc) {
-      if (doc.get("totalPoints") >= points) {
-        doc.get("totalPoints");
+    .then(function(doc){
+        if (doc.get("totalPoints") >= reward[1]) { //they have enough points to buy reward
+            setEnoughPoints(true) 
+            deductPoints(db, groupID, reward, setPointsDisplay)
+        } else {
+            setEnoughPoints(false)
+        }
+        setIsShowing(true);
+         
+    })
+}
 
-        let balance = doc.get("totalPoints") - points;
-        console.log("Your balance is " + balance);
 
-        db.collection("Groups") //deducts points in Groups collection for current user
+function deductPoints(db, groupID, reward, setPointsDisplay){ //only called if they have enough points
+  db.collection("Groups")
+  .doc(groupID)
+  .collection("Users")
+  .doc(sessionStorage.getItem("user"))
+  .get()
+  .then(function(doc){
+      console.log("deducting" + reward[1] + "points")
+      let balance = doc.get("totalPoints") - reward[1];
+
+      db.collection("Groups") //deducts points in Groups collection for current user
           .doc(groupID)
           .collection("Users")
           .doc(sessionStorage.getItem("user"))
           .update({
-            totalPoints: balance
+              totalPoints: balance
           });
-        setPointsDisplay(true);
-        revealSuccessTask();
-        //this does setShowModal(true); as set in the function handleCheckboxClick
-      } else {
-        console.log(
-          "You do not have enough points to claim this reward. Keep working!"
-        );
-        revealFailTask();
-        //there should be a function here that sets a state that shows a different component
-      }
-    });
+          setPointsDisplay(true);
+           
+  })
+
 }
 
-function PurchaseSuccess(props) {
-  return (
-      <Modal show={props.showSuccess} onHide={props.hideSuccess}>
-        <Modal.Header closeButton>
-          <Modal.Title>Success</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>You have sucessfully purchased this reward</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={props.hideSuccess}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-  );
-}
-function PurchaseFail(props) {
-  return (
-      <Modal show={props.showFailure} onHide={props.hideFailure}>
-        <Modal.Header closeButton>
-          <Modal.Title>Failure</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          You do not have enough points to purchase this reward
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={props.hideFailure}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-  );
-}
+  
 
 export default Reward;
