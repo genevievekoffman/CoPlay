349c59f96ef165f43656b8b8a4fb5c034434e32a
Genevieve Koffman <genkoffman@gmail.com>
2020-01-26 13:02:47 +0200

    addedPopup

---
+++ coplay/src/Popups/CreateGroup/CreateGroup.js
@@ -0,0 +1,86 @@
+import React from "react";
+import "./CreateGroup.css";
+
+import creategrouptop from "../../Sketches/CreateGroupTop.svg";  
+
+function CreateGroup(props){ 
+    const{isShowing2, setIsShowing2, db, setCounter, setGroupsList} = props;
+
+    if(isShowing2){
+        return(
+            <div>
+                <div className = "backgrnd" onClick = {() => {setIsShowing2(false)}}></div>
+                <div className = "inner_msg">
+                    <form onSubmit = {event => {
+                        addGroup(event, db, setGroupsList, setCounter, db);
+                        setIsShowing2(false)
+                    }}>
+                        <img src={creategrouptop} className="creategrouptop" alt="creategrouptop" />
+                        <br></br>
+                        <h5>Group Title</h5>
+                        <input type="text" name="title" className = "createGroupText"></input>
+                        <br></br>
+                        <input type="submit" value="Join" className = "create_btn"></input>
+                    </form>
+                </div>
+
+            </div>
+             
+        )
+
+    } else {
+        return null
+    }
+     
+}
+
+export default CreateGroup;
+
+function addGroup(event, db, setGroupsList, setCounter) {
+  event.preventDefault();
+  let ID;
+
+  console.log("Group added");
+  let title = event.target.elements.title.value;
+  if (title === "") {
+    alert("Must enter a title");
+  } else {
+    console.log("The Group " + title + " has been added");
+    ID = "a" + Date.now();
+    db.collection("Groups")
+      .doc(ID)
+      .set({
+        name: title,
+        ID: ID
+      });
+    addGroupUser(db, title, ID);
+    addUserToGroup(db, sessionStorage.getItem("user"), ID);
+  }
+
+  event.target.elements.title.value = "";
+  setCounter(0);
+}
+
+function addGroupUser(db, title, ID) {
+  db.collection("Users")
+    .doc(sessionStorage.getItem("user"))
+    .collection("Groups")
+    .doc(ID)
+    .set({
+      name: title,
+      ID: ID
+    });
+}
+
+function addUserToGroup(db, username, groupID) {
+  db.collection("Groups")
+    .doc(groupID)
+    .collection("Users")
+    .doc(username)
+    .set({
+      username: username,
+      admin: true,
+      totalPoints: 0,
+      leaderBoardPoints: 0 //this is the total points the user has ever earned(can only increase)
+    });
+}
--- coplay/src/view/pages/Groups/Groups.js
+++ coplay/src/view/pages/Groups/Groups.js
@@ -6,6 +6,8 @@ import Group from "../../Group/Group";
 import JoinGroup from "../../../Popups/JoinGroup/JoinGroup";
 import JoinGroupFails from "../../../Popups/JoinGroup/JoinGroupFails"
 
+import CreateGroup from "../../../Popups/CreateGroup/CreateGroup";
+
 
 //component
 import twoCircles from "../../../Sketches/Twocircles.svg";
@@ -18,8 +20,9 @@ function Groups(props) {
   const [isShowingFails, setIsShowingFails] = useState(false)
  
 
-  //for joinGroup popup
+  //for joinGroup and CreateGroup popup
   const [isShowing, setIsShowing] = useState(false);
+  const [isShowing2, setIsShowing2] = useState(false);
 
   if (counter < 1) {
     fetchMyGroups(db, setGroupsList, setCounter);
@@ -38,14 +41,23 @@ function Groups(props) {
 
       <header className="groupPageTitle">My Groups</header>
       <div className="groupButtons">
-        {
+        {/* {
           <AddGroupForm
             db={db}
             setGroupsList={setGroupsList}
             setCounter={setCounter} 
              
           />
-        }
+        } */}
+
+        <CreateGroup
+          isShowing2 = {isShowing2}
+          setIsShowing2 = {setIsShowing2}
+          db = {db}
+          setCounter = {setCounter}
+          setGroupsList = {setGroupsList}
+        />
+
         <JoinGroup
           isShowing={isShowing} 
           setIsShowing={setIsShowing}
@@ -57,16 +69,16 @@ function Groups(props) {
         isShowingFails = {isShowingFails}
         setIsShowingFails={setIsShowingFails}
         />
+        <div className = "create_div" onClick = { () => {setIsShowing2(true)}}>
+          Create
+        </div>    
         
         <div className = "join_div" onClick = { () => {setIsShowing(true)}}>
           Join
         </div>       
+        
          
-        {/* <JoinGroupForm
-          db={db}
-          setGroupsList={setGroupsList}
-          setCounter={setCounter}
-        /> */}
+         
       </div>
       
       {circlesClicked === 1 ? ( //1->they clicked it (group ID's are shown)
@@ -128,125 +140,7 @@ function fetchMyGroups(db, setGroupsList, setCounter) {
       setCounter(1); 
     });
 }
-
-function addGroupUser(db, title, ID) {
-  db.collection("Users")
-    .doc(sessionStorage.getItem("user"))
-    .collection("Groups")
-    .doc(ID)
-    .set({
-      name: title,
-      ID: ID
-    });
-}
-
-function addUserToGroup(db, username, groupID) {
-  db.collection("Groups")
-    .doc(groupID)
-    .collection("Users")
-    .doc(username)
-    .set({
-      username: username,
-      admin: true,
-      totalPoints: 0,
-      leaderBoardPoints: 0 //this is the total points the user has ever earned(can only increase)
-    });
-}
-
-function addGroup(event, db, setGroupsList, setCounter) {
-  event.preventDefault();
-  let ID;
-
-  console.log("Group added");
-  let title = event.target.elements.title.value;
-  if (title === "") {
-    alert("Must enter a title");
-  } else {
-    console.log("The Group " + title + " has been added");
-    ID = "a" + Date.now();
-    db.collection("Groups")
-      .doc(ID)
-      .set({
-        name: title,
-        ID: ID
-      });
-    addGroupUser(db, title, ID);
-    addUserToGroup(db, sessionStorage.getItem("user"), ID);
-  }
-
-  event.target.elements.title.value = "";
-  fetchMyGroups(db, setGroupsList, setCounter);
-}
-
-function AddGroupForm(props) {
-  const { db, setGroupsList, setCounter } = props;
-  return (
-    <div>
-      <button
-        className="createBtn"
-        data-toggle="modal"
-        data-target="#myModal"
-        id="plus"
-        // className="addGroupButton"
-      >
-        Create
-      </button>
-
-      <div className="row">
-        <div className="col-md-12">
-          <div className="modal fade" id="myModal">
-            <div className="modal-dialog">
-              <div className="modal-content">
-                <div className="modal-header">
-                  <h3>Add a Group</h3>
-                </div>
-                <form
-                  onSubmit={event => {
-                    addGroup(event, db, setGroupsList, setCounter);
-                    //xxx
-                  }}
-                >
-                  <div className="modal-body">
-                    <input
-                      type="text"
-                      name="title"
-                      placeholder="Title"
-                      id="Title"
-                      className="m-1"
-                    />
-                  </div>
-                  <div className="modal-footer" id = "BottomButtons">
-                    <button
-                      className="btn btn-secondary btn-sm"
-                      // id="cancell"
-                      value="Cancel"
-                      type="button"
-                      data-dismiss="modal"
-                    >
-                      Cancel
-                    </button>
-                    <button
-                      className="btn btn-primary btn-sm"
-                      type="submit"
-                      // id="savee"
-                      value="Save"
-                      name="save"
-                      data-toggle="modal"
-                      data-target="#myModal"
-                    >
-                      Save
-                    </button>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
-
+ 
 
 function twoCirclesClicked(setCirclesClicked, circlesClicked) {
   if (circlesClicked === 0){
